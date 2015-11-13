303f50434182926b27ec5fec9e7830abe053d9c3
Billy Springsteen <william.springsteen14@imperial.ac.uk>
2015-11-13 17:05:54 +0000

    Added another test

--- src/test/java/ic/doc/HigherOrderListTest.java
+++ src/test/java/ic/doc/HigherOrderListTest.java
@@ -2,6 +2,8 @@ package ic.doc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
@@ -69,6 +71,33 @@ public class HigherOrderListTest {
       }
     }, 0);
     assertThat(addAll, is(10));
+  }
+  
+  @Test
+  public void doesMappableListRunWithThreadSleeping() {
+    ArrayList<Integer> nonMappableList = new ArrayList<Integer>(
+        Arrays.asList(1, 2, 3, 4));
+    MappableList<Integer> list = new MappableList<Integer>(nonMappableList);
+    
+    UnaryFunction<Integer> slowSquare = new UnaryFunction<Integer>() {
+      
+      @Override
+      public Integer applyTo(Integer elem) {
+        try {
+          Thread.sleep(2000);
+        } catch (InterruptedException e) {
+          System.out.println("Error - Interrupt Exception");
+        }
+        return elem*elem;
+      }
+      
+    };
+    
+    try {
+      List<Integer> squares = list.doFunction(slowSquare);
+    } catch (Exception e) {
+      System.out.println("Exception occured.");
+    }
   }
   
 }
